66fd6343607d525504c11f5dd0b7e765d5d2e021
it2022031 <it2022031@hua.gr>
2026-01-20 00:19:31 +0200

    xana allages stis diefthinsis

--- vm/vagrant/jenkins/jenkinsfiles/seed_vms.groovy
+++ vm/vagrant/jenkins/jenkinsfiles/seed_vms.groovy
@@ -6,29 +6,29 @@ pipeline {
     stages {
         stage('Checkout') { steps { checkout scm } }
 
-        stage('Seed DB (Docker)') {
+        stage('Seed DB (VMs)') {
             steps {
                 sh '''
           set -e
           cd vm/vagrant
-          ansible-playbook -i hosts_jenkins.ini docker/playbooks/docker_seed_like_k8s.yml
+          ansible-playbook -i hosts_jenkins.ini playbooks/vm_seed_like_k8s.yml
         '''
             }
         }
 
-        stage('Load Photos (Docker)') {
+        stage('Load Photos (VMs)') {
             steps {
                 sh '''
           set -e
           cd vm/vagrant
-          ansible-playbook -i hosts_jenkins.ini docker/playbooks/docker_load_photos_like_k8s.yml
+          ansible-playbook -i hosts_jenkins.ini playbooks/vm_load_photos_like_k8s.yml
         '''
             }
         }
     }
 
     post {
-        success { echo '✅ seed-docker: OK' }
-        failure { echo '❌ seed-docker: FAILED' }
+        success { echo '✅ seed-vms: OK' }
+        failure { echo '❌ seed-vms: FAILED' }
     }
 }
