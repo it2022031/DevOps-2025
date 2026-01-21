5d37c97f96a5d1726685c675acc47e975fd0d0b1
it2022031 <it2022031@hua.gr>
2026-01-21 05:42:39 +0200

    sosto token kai allagi stis diefthinis gia na anevazi packahes images

--- ci/jenkins/jenkinsfiles/build_push_images.groovy
+++ ci/jenkins/jenkinsfiles/build_push_images.groovy
@@ -4,20 +4,19 @@ pipeline {
 
     environment {
         ANSIBLE_HOST_KEY_CHECKING = "False"
+        ANSIBLE_CONFIG = "infra/ansible/ansible-jenkins.cfg"
     }
 
     stages {
-        stage('Checkout') {
-            steps { checkout scm }
-        }
+        stage('Checkout') { steps { checkout scm } }
 
         stage('Build & Push images (on dockerhost)') {
             steps {
                 withCredentials([string(credentialsId: 'ghcr_token', variable: 'GHCR_TOKEN')]) {
                     sh '''
             set -e
-            cd vm/vagrant
-            ansible-playbook -i hosts_jenkins.ini docker/playbooks/build_push_images.yml \
+            ansible-playbook -i infra/inventories/hosts_jenkins.ini ansible/docker/playbooks/build_push_images.yml \
+              --limit docker \
               -e "ghcr_token=$GHCR_TOKEN"
           '''
                 }
