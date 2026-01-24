7ce49566938b90d8bd5c949b5a325dc58ee10914
it2022031 <it2022031@hua.gr>
2026-01-24 02:36:07 +0200

    asdas

--- ci/jenkins/jobdsl/jobs_cloud.groovy
+++ ci/jenkins/jobdsl/jobs_cloud.groovy
@@ -1,68 +1,43 @@
-// Job DSL (cloud): creates pipeline jobs from SCM (this repo)
-
 def repoUrl = 'https://github.com/it2022031/DevOps-2025.git'
 def branch  = '*/main'
 
-// Αν το repo είναι PRIVATE, βάλε credentialsId και δημιουργησέ το στο Jenkins (Manage Credentials)
-// def gitCreds = 'github-creds-id'   // <- άλλαξε το
+// Αν χρειαστεί για private repo, βάλε credentialsId εδώ:
 def gitCreds = null
+// def gitCreds = 'github-creds-id'
 
-def cpsScmDef = { String scriptPath ->
-    cpsScm {
-        scm {
-            git {
-                remote {
-                    url(repoUrl)
-                    if (gitCreds) {
-                        credentials(gitCreds)
-                    }
-                }
-                branches(branch)
-            }
-        }
-        scriptPath(scriptPath)
-        lightweight(true)
-    }
-}
-
-// (Optional) Folder για να είναι μαζεμένα
 folder('DS-2025') {
     displayName('DS-2025')
     description('Pipelines for DevOps-2025 / DS-2025 project')
 }
 
-// 1) infra-check
-pipelineJob('DS-2025/infra-check') {
-    definition { cpsScmDef('ci/jenkins/jenkinsfiles/test.groovy') }
+def makePipelineJob = { String name, String scriptPath ->
+    pipelineJob(name) {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url(repoUrl)
+                            if (gitCreds) {
+                                credentials(gitCreds)
+                            }
+                        }
+                        branches(branch)
+                    }
+                }
+                scriptPath(scriptPath)
+                lightweight(true)
+            }
+        }
+    }
 }
 
-// 2) deploy targets
-pipelineJob('DS-2025/deploy-vms') {
-    definition { cpsScmDef('ci/jenkins/jenkinsfiles/deploy_vms.groovy') }
-}
-
-pipelineJob('DS-2025/deploy-docker') {
-    definition { cpsScmDef('ci/jenkins/jenkinsfiles/deploy_docker.groovy') }
-}
-
-pipelineJob('DS-2025/deploy-k8s') {
-    definition { cpsScmDef('ci/jenkins/jenkinsfiles/deploy_k8s.groovy') }
-}
-
-// 3) build/push images
-pipelineJob('DS-2025/build-push-images') {
-    definition { cpsScmDef('ci/jenkins/jenkinsfiles/build_push_images.groovy') }
-}
-
-// 4) seed data jobs
-pipelineJob('DS-2025/seed-vms') {
-    definition { cpsScmDef('ci/jenkins/jenkinsfiles/seed_vms.groovy') }
-}
-
-pipelineJob('DS-2025/seed-docker') {
-    definition { cpsScmDef('ci/jenkins/jenkinsfiles/seed_docker.groovy') }
-}
-
-pipelineJob('DS-2025/seed-k8s') {
-    definition { cpsScmDef('ci/jenkins/jenkinsfiles/seed_k8s.groovy') }
-}
+// Jobs
+makePipelineJob('DS-2025/infra-check',       'ci/jenkins/jenkinsfiles/test.groovy')
+makePipelineJob('DS-2025/deploy-vms',        'ci/jenkins/jenkinsfiles/deploy_vms.groovy')
+makePipelineJob('DS-2025/deploy-docker',     'ci/jenkins/jenkinsfiles/deploy_docker.groovy')
+makePipelineJob('DS-2025/deploy-k8s',        'ci/jenkins/jenkinsfiles/deploy_k8s.groovy')
+makePipelineJob('DS-2025/build-push-images', 'ci/jenkins/jenkinsfiles/build_push_images.groovy')
+makePipelineJob('DS-2025/seed-vms',          'ci/jenkins/jenkinsfiles/seed_vms.groovy')
+makePipelineJob('DS-2025/seed-docker',       'ci/jenkins/jenkinsfiles/seed_docker.groovy')
+makePipelineJob('DS-2025/seed-k8s',          'ci/jenkins/jenkinsfiles/seed_k8s.groovy')
