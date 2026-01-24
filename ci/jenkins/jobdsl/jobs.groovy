8ba0346c94f7f726fb2769c1909e8e2f4fd58867
it2022031 <it2022031@hua.gr>
2026-01-24 02:39:33 +0200

    a

--- ci/jenkins/jobdsl/jobs.groovy
+++ ci/jenkins/jobdsl/jobs.groovy
@@ -1,103 +1,29 @@
-// Job DSL: creates pipeline jobs from SCM (your repo)
-
 def repoUrl = 'https://github.com/it2022031/DevOps-2025.git'
-def branch = '*/main'
+def branch  = '*/main'
 
-// 1) infra-check (ping)
-pipelineJob('infra-check') {
+// Αν είναι private repo, βάλε credentialsId εδώ (αλλιώς άστο null)
+def gitCreds = null
+// def gitCreds = 'github-creds-id'
+
+folder('DS-2025') {
+    displayName('DS-2025')
+    description('Pipelines for DevOps-2025 / DS-2025 project')
+}
+
+pipelineJob('DS-2025/ping-vms') {
     definition {
         cpsScm {
             scm {
                 git {
-                    remote { url(repoUrl) }
+                    remote {
+                        url(repoUrl)
+                        if (gitCreds) { credentials(gitCreds) }
+                    }
                     branches(branch)
                 }
             }
-            scriptPath('ci/jenkins/jenkinsfiles/test.groovy')
+            scriptPath('ci/jenkins/jenkinsfiles/ping_vms.groovy')
             lightweight(true)
         }
     }
 }
-
-// 2) deploy-vms
-pipelineJob('deploy-vms') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote { url(repoUrl) }
-                    branches(branch)
-                }
-            }
-            scriptPath('ci/jenkins/jenkinsfiles/deploy_vms.groovy')
-            lightweight(true)
-        }
-    }
-}
-pipelineJob('deploy-docker') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote { url(repoUrl) }
-                    branches(branch)
-                }
-            }
-            scriptPath('ci/jenkins/jenkinsfiles/deploy_docker.groovy')
-            lightweight(true)
-        }
-    }
-}
-pipelineJob('deploy-k8s') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote { url(repoUrl) }
-                    branches(branch)
-                }
-            }
-            scriptPath('ci/jenkins/jenkinsfiles/deploy_k8s.groovy')
-            lightweight(true)
-        }
-    }
-}
-pipelineJob('build-push-images') {
-    definition {
-        cpsScm {
-            scm { git { remote { url(repoUrl) }; branches(branch) } }
-            scriptPath('ci/jenkins/jenkinsfiles/build_push_images.groovy')
-            lightweight(true)
-        }
-    }
-}
-pipelineJob('seed-vms') {
-    definition {
-        cpsScm {
-            scm { git { remote { url(repoUrl) }; branches(branch) } }
-            scriptPath('ci/jenkins/jenkinsfiles/seed_vms.groovy')
-            lightweight(true)
-        }
-    }
-}
-
-pipelineJob('seed-docker') {
-    definition {
-        cpsScm {
-            scm { git { remote { url(repoUrl) }; branches(branch) } }
-            scriptPath('ci/jenkins/jenkinsfiles/seed_docker.groovy')
-            lightweight(true)
-        }
-    }
-}
-
-pipelineJob('seed-k8s') {
-    definition {
-        cpsScm {
-            scm { git { remote { url(repoUrl) }; branches(branch) } }
-            scriptPath('ci/jenkins/jenkinsfiles/seed_k8s.groovy')
-            lightweight(true)
-        }
-    }
-}
-
